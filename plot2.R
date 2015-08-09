e8837446ab340725bd080d0aaada1cc261f879a6
ylcolala <ylcolala@gmail.com>
2015-08-09 20:32:23 +0800

    no message

--- plot2.R
+++ plot2.R
@@ -2,6 +2,6 @@ household<-read.table("household_power_consumption.txt", head=TRUE, stringsAsFac
                       na.strings="?",sep=';')
 household<-subset(household, household$Date %in% c("1/2/2007","2/2/2007"))
 datetime <- strptime(paste(household$Date, household$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-plot(datetime, as.numeric(household$Global_active_power),type="l")
+plot(datetime, as.numeric(household$Global_active_power),type="l",ylab="Global active power")
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
